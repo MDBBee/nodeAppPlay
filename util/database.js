bc4eb289cac2bab400cc7201267f2176a618b3f5
MDBBee <bobbyugbebor@gmail.com>
2024-10-07 03:18:47 +0300

    DBM continuation

--- util/database.js
+++ util/database.js
@@ -6,14 +6,3 @@ const sequelize = new Sequelize("node-1", "root", "Brown27A1", {
 });
 
 module.exports = sequelize;
-
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-1",
-//   password: "Brown27A1",
-// });
-
-// module.exports = pool.promise();
